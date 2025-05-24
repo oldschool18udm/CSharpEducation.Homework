0cabd821b6d81353182a436931b46ab71a2b4141
Sofia <sofiaozhgihina@yandex.ru>
2025-05-24 15:55:52 +0400

    домашнее задание №3

---
+++ PhoneBookProgram/Program.cs
@@ -0,0 +1,70 @@
+﻿using PhonebookLibrary;
+using System.IO;
+
+namespace PhoneBookProgram;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string command = "", name, phone;
+        string bookfile;
+        Console.Write("Укажите имя файла телефонной книжки: ");
+        bookfile = Console.ReadLine();
+        Phonebook phonebook = new Phonebook(bookfile);
+        if (!File.Exists(bookfile))
+        {
+            phonebook.CreatePB();
+        }
+        Console.Clear();
+        while (command != "6")
+        {
+            Console.WriteLine(@"        Меню:
+                    1. Показать Содержимое телефонной Книжки
+                    2. Создать контакт 
+                    3. Найти контакт по номеру телефона
+                    4. Найти контакт по имени
+                    5. Удалить контакт по имени
+                    6. Выход из программы
+                    ");
+            command = Console.ReadLine();
+            Console.Clear();
+            switch (command)
+            {
+                case "1":
+                    Console.WriteLine("Содержимое телефонной Книжки:");
+                    phonebook.ShowPB();
+                    break;
+                case "2":
+                    Console.WriteLine("Добавляем новый контакт в книжку:");
+                    Console.Write("Введите имя: ");
+                    name = Console.ReadLine();
+                    Console.Write("Введите номер телефона: ");
+                    phone = Console.ReadLine();
+                    Abonent abonent1 = new Abonent(name, phone);
+                    phonebook.AddItem(abonent1);
+                    break;
+                case "3":
+                    Console.WriteLine("Поиск контакта по номеру телефона:");
+                    Console.Write("Введите номер телефона: ");
+                    phone = Console.ReadLine();
+                    Console.WriteLine(phonebook.FindItemByPhone(phone));
+                    break;
+                case "4":
+                    Console.WriteLine("Поиск контакта по имени");
+                    Console.Write("Введите имя: ");
+                    name = Console.ReadLine();
+                    Console.WriteLine(phonebook.FindItemByName(name));
+                    break;
+                case "5":
+                    Console.WriteLine("Удаление контакта по имени");
+                    Console.Write("Введите имя: ");
+                    name = Console.ReadLine();
+                    phonebook.DeleteItem(name);
+                    break;
+                case "6": Console.WriteLine("Работа программы завершена"); break;
+                default: Console.WriteLine("Неверная команда"); break;
+            }
+        }
+    }
+}
---
+++ PhonebookLibrary/Phonebook.cs
@@ -0,0 +1,145 @@
+﻿namespace PhonebookLibrary;
+
+public class Phonebook
+{
+    public string path;
+    public Abonent abonent;
+    public int lastId = 0;
+
+    public Phonebook(string path)
+    {
+        this.path = path;
+        this.abonent = abonent;
+        this.lastId = lastId;
+        
+    }
+
+    public  void CreatePB()
+    {
+        // string[] lines = ["1;маша;89124567896", "2;даша;89561234578", "3;наташа;89127418526", "4;саша;89127411234"];
+        using (StreamWriter sw = new StreamWriter(this.path, false))
+        {
+        }
+        // {
+        //     foreach (string line in lines)
+        //     {
+        //         sw.WriteLineAsync(line);
+        //     }
+        // }
+
+        // this.lastId = lines.Length;
+    }
+
+    public void ShowPB()
+    {
+        using (StreamReader sr = new StreamReader(path))
+        {
+            string line = sr.ReadLine();
+            while (line != null)
+            {
+                Console.WriteLine(line);
+                line = sr.ReadLine();
+            }
+        }
+    }
+
+    public void AddItem(Abonent abonent)
+    {
+        bool find = false;
+        using (StreamReader sr = new StreamReader(path))
+        {
+            string lineToFind = sr.ReadLine();
+
+            while (lineToFind != null)
+            {
+                if (lineToFind.Split(";")[1] == abonent.name && lineToFind.Split(";")[2] == abonent.phone)
+                {
+                    find = true;
+                    break;
+                }
+
+                lineToFind = sr.ReadLine();
+            }
+        }
+
+        using (StreamWriter sw = new StreamWriter(path, true))
+        {
+            if (!find)
+            {
+                this.lastId++;
+                string line = this.lastId + ";" + abonent.name + ";" + abonent.phone;
+                sw.WriteLineAsync(line);
+            }
+            else
+            {
+                Console.WriteLine("Пользователь с такими данными уже существует");
+            }
+        }
+    }
+
+    public string FindItemByPhone(string phone)
+    {
+        string result = "";
+        using (StreamReader sr = new StreamReader(path))
+        {
+            string line = sr.ReadLine();
+            while (line != null)
+            {
+                if (line.Split(";")[2] == phone)
+                {
+                    result += $"\n{line.Split(";")[2]}-{line.Split(";")[1]}";
+                }
+
+                line = sr.ReadLine();
+            }
+        }
+
+        if (result != "")
+            return result;
+        return "Не найдено пользователей с таким номером телефона";
+    }
+    public string FindItemByName(string name)
+    {
+        string result = "";
+        using (StreamReader sr = new StreamReader(path))
+        {
+            string line = sr.ReadLine();
+            while (line != null)
+            {
+                if (line.Split(";")[1] == name)
+                {
+                    result += $"\n{line.Split(";")[1]}-{line.Split(";")[2]}";
+                }
+
+                line = sr.ReadLine();
+            }
+        }
+        if (result != "")
+            return result;
+        return "Не найдено пользователей с таким именем";
+    }
+    public  void DeleteItem(string name)
+    {
+        List<string>people = new List<string>();
+        using (StreamReader sr = new StreamReader(path))
+        {
+            string line = sr.ReadLine();
+            while (line != null)
+            {
+                people.Add(line);
+                line = sr.ReadLine();
+            }
+        }
+        using (StreamWriter sw = new StreamWriter(path, false))
+        {
+            foreach (var line in people)
+            {
+                if (line.Split(";")[1] != name)
+                {
+                    sw.WriteLine(line);
+                }
+                
+            }
+        }
+    }
+}
